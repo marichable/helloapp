066df204b47698002796c3f0d807629655d1da80
Marich Bardhan <marichbardhan@Marichs-MacBook-Pro-3.local>
2025-07-21 11:31:53 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from pathlib import Path
+
+app = FastAPI()
+
+BASE_DIR = Path(__file__).resolve().parent
+templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
+
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.get("/second", response_class=HTMLResponse)
+async def second_page(request: Request):
+    return templates.TemplateResponse("second.html", {"request": request})
